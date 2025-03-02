11bb630d809c12e1749ab17aa954e32ebb333dbe
Jarede F. Silva <jarede.silva@bistek.com.br>
2025-03-02 18:58:37 -0300

    atualizado dashboard

--- nvim/lua/plugins/dashboard.lua
+++ nvim/lua/plugins/dashboard.lua
@@ -6,11 +6,43 @@ return {
         sections = {
           {
             section = "terminal",
-            cmd = "pokemon-colorscripts -r --no-title; sleep .1",
+            cmd = "fortune -s | cowsay",
+            padding = 1,
+            indent = 8,
+            random = 10,
             height = 30,
-            random = 1000,
+            width = 60,
           },
           { section = "startup" },
+          {
+            section = "terminal",
+            cmd = "pokemon-colorscripts -r --no-title; sleep .1",
+            indent = 4,
+            height = 30,
+            random = 1000,
+            pane = 2,
+          },
+          function()
+            local in_git = Snacks.git.get_root() ~= nil
+            local cmds = {
+              {
+                icon = " ",
+                title = "Git Status",
+                cmd = "git --no-pager diff --stat -B -M -C",
+                height = 10,
+              },
+            }
+            return vim.tbl_map(function(cmd)
+              return vim.tbl_extend("force", {
+                pane = 2,
+                section = "terminal",
+                enabled = in_git,
+                padding = 1,
+                ttl = 5 * 60,
+                indent = 3,
+              }, cmd)
+            end, cmds)
+          end,
         },
       },
     },
