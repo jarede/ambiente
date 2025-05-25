9d0bd5d68912c1421c1024d6681ee060e8a66bca
Jarede F. Silva <jarede.silva+github@gmail.com>
2025-05-25 12:47:57 -0300

    comandos do bucket

--- brv/testar_devcontainer.sh
+++ brv/testar_devcontainer.sh
@@ -2,3 +2,8 @@
 
 fd -e py | entr -c python3.11 -m unittest tests.asap_tests.views.test_view.TestView.test_update
 
+# s3 bucket
+# listar os buckets
+aws --endpoint-url=http://localhost:4566 s3 ls
+# listar o conteúdo dos buckets
+aws --endpoint-url=http://localhost:4566 s3 ls s3://[bucket] --recursive
