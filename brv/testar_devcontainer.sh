baa35c4b3094c1d0dd264a2b7fdb433ff677caab
Jarede F. Silva <jarede.silva+github@gmail.com>
2025-05-31 12:42:09 -0300

    Update testar_devcontainer.sh
    snippet comando pewwee

--- brv/testar_devcontainer.sh
+++ brv/testar_devcontainer.sh
@@ -7,3 +7,8 @@ fd -e py | entr -c python3.11 -m unittest tests.asap_tests.views.test_view.TestV
 aws --endpoint-url=http://localhost:4566 s3 ls
 # listar o conteúdo dos buckets
 aws --endpoint-url=http://localhost:4566 s3 ls s3://[bucket] --recursive
+
+# select do pewee
+c, v = query.sql()
+command = c.replace("%s", "'{}'")
+print(command.format(*v))
